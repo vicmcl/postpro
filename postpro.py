39ce19d15c0fa3fac7179e05786dc90793df030e
Victorien Michel <victorienmichel3@gmail.com>
2023-07-12 17:10:07 +0200

    Added comments to plot_data funcs

--- postpro.py
+++ postpro.py
@@ -44,43 +44,67 @@ def reload():
 # * ===================================================================================================
 
 def gather_runs(runs):
+    # Convert CSV data to DataFrame
     csv_df = tb._csv_postpro_to_df()
-    runs_dir = []
 
+    # Find runs in the specified directory
+    runs_dir = []
     runs_dir += tb._find_runs(runs)
 
+    # Count the number of unique run directories
     runs_nb = len({os.path.basename(r) for r in runs_dir})
 
+    # Raise an error if no runs are found
     if len(runs_dir) == 0:
         raise ValueError(f"No run found with {tb._bred}'{runs}'{tb._reset}.")
 
     return runs_dir, runs_nb, csv_df
 
+# * ===================================================================================================
+
 def gather_data(runs_dir, specdir, probe, **kwargs):
+    # List to store processed run DataFrames
     run_pp_df_list = []
 
+    # Iterate over each run directory
     for run_path in runs_dir:
         print(f"\n{tb.bmag}--------\n# {os.path.basename(run_path)}\n--------{tb.reset}\n")
+        
+        # Get data from the run and add it to the list
         run_pp_df_list += [data for data in tb._get_data_from_run(run_path, specdir=specdir,
                                                                   probe=probe, **kwargs)]
+        
+        # Raise an error if no data is found for the input
         if not run_pp_df_list:
             raise ValueError("No data found with such input.")
 
     return run_pp_df_list
 
+# * ===================================================================================================
+
 def plot_time_data(ax, df, handle_prefix, frmt_legend):
+    # Iterate over each column (excluding 'Time') in the DataFrame
     for col in [c for c in df.columns if c != 'Time']:
         handle = f"{handle_prefix}{col}{frmt_legend}"
+        
+        # Plot the data as a line plot
         sns.lineplot(data=df, x='Time', y=col, label=handle, ax=ax, linewidth=cst.LINEWIDTH)
         sns.despine(left=True)
-        yield handle    
+        
+        # Yield the handle for each iteration
+        yield handle
+
+# * ===================================================================================================
 
 def plot_freq_data(ax, df, handle_prefix, frmt_legend, sampling_rate, **kwargs):
+    # Iterate over each column (excluding 'Time') in the DataFrame
     for col in [c for c in df.columns if c != 'Time']:
         frmt_col = f"{handle_prefix}{col}"
         handle = f"{frmt_col}{frmt_legend}"
 
         print(f"Calculating FFT for {tb.bmag}{col}{tb.reset}...")
+        
+        # Calculate the FFT and frequency values
         signal_fft = fft(df[col].values)
         freqs = fftfreq(len(df[col])) * sampling_rate
 
@@ -90,22 +114,31 @@ def plot_freq_data(ax, df, handle_prefix, frmt_legend, sampling_rate, **kwargs):
         if "lowpass" in kwargs:
             pos_freqs = pos_freqs[pos_freqs <= int(kwargs.get("lowpass"))]
 
+        # Plot the frequency data as a line plot
         sns.lineplot(x=pos_freqs, y=normalized_spectrum[:len(pos_freqs)], label=handle, ax=ax, linewidth=cst.LINEWIDTH)
+        
+        # Yield the handle for each iteration
         yield handle
-    
+
+# * ===================================================================================================
+
 def set_figure_params(probe, specdir):
+    # Create a new figure and axis with specified parameters
     _, ax = plt.subplots(figsize=(12, 27 / 4))
     ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
     plt.grid(axis='y', linewidth=0.1, color='00000')
     plt.grid(axis='x', linewidth=0)
 
+    # Set y-axis scale to logarithmic if probe and specdir are None
     if probe is None and specdir is None:
         plt.yscale('log')
 
     return ax
 
+# * ===================================================================================================
+
 def format_legend(ax, handles):
-    
+    # Set legend properties
     ax.legend(
         loc='upper center',
         bbox_to_anchor=[0.5, -0.2],
@@ -116,7 +149,10 @@ def format_legend(ax, handles):
         fontsize=12
     )
 
+# * ===================================================================================================
+
 def set_axis_labels(ax, freq=False, unit=None):
+    # Set axis labels based on frequency or time data
     if freq:
         ax.set_ylabel("Normalized Amplitude", labelpad=10, fontsize=15)
         ax.set_xlabel("Frequency (Hz)", labelpad=18, fontsize=15)
@@ -124,13 +160,18 @@ def set_axis_labels(ax, freq=False, unit=None):
         ax.set_xlabel("Iterations | Time (s)", labelpad=18, fontsize=15)
         ax.set_ylabel(unit, labelpad=10, fontsize=15)
 
+# * ===================================================================================================
+
 def plot_data(runs, *, specdir: str, probe: str = None, freq: bool = False, **kwargs):
+    # Gather runs, run data, and CSV DataFrame
     runs_dir, runs_nb, csv_df = gather_runs(runs)
     run_pp_df_list = gather_data(runs_dir, specdir, probe, **kwargs)
     
+    # Set up the figure parameters
     ax = set_figure_params(probe, specdir)
     handle_prefix = "Probe " if probe is not None else ""
 
+    # Iterate over each processed run DataFrame
     for data in run_pp_df_list:
         run_id, pp_dir, df = data.values()
         frmt_legend = " | " + run_id if runs_nb > 1 else ""
@@ -139,20 +180,28 @@ def plot_data(runs, *, specdir: str, probe: str = None, freq: bool = False, **kw
             sampling_rate = len(df["Time"]) / df["Time"].iloc[-1]
             set_axis_labels(ax, freq=True)
             print(f"\n{tb.bmag}------------\n# FFT {run_id}\n------------{tb.reset}\n")
+            
+            # Plot frequency data and yield handles
             handles = [h for h in plot_freq_data(ax, df, handle_prefix, frmt_legend, sampling_rate, **kwargs)]
         else:
             unit = tb._get_unit(df=df, pp_dir=pp_dir, probe=probe, csv_df=csv_df, **kwargs)
             set_axis_labels(ax, unit=unit)
+            
+            # Plot time data and yield handles
             handles = [h for h in plot_time_data(ax, df, handle_prefix, frmt_legend)]
 
-    if unit is None: plt.gca().set_ylabel(None)
+    if unit is None:
+        plt.gca().set_ylabel(None)
+    
+    # Format the legend and set the title if specified
     format_legend(ax, handles)
-
     if 'title' in kwargs:
         title = kwargs.get('title')
         ax.set_title(title, fontsize=20)
 
     print('\nDisplaying the figure...\n')
+    
+    # Adjust figure layout and display the figure
     plt.tight_layout()
     plt.show()
 
